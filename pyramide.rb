30d73e73d7a0b34e1e42a80469b5b7b18ff1039f
Franky <dasylva.f@gmail.com>
2018-06-12 05:01:23 +0200

    Ajout de commentaires

--- pyramide.rb
+++ pyramide.rb
@@ -1,22 +1,35 @@
 puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?
 Merci d'entrer un nombre compris entre 1 et 25 !"
+
 print "> "
 num = gets.chomp.to_i
+
+# Debut du loop pour obtenir un chiffre entre 1 et 25
+
 while num < 1 or num > 25
-  puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?
+	puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?
 Merci d'entrer un nombre compris entre 1 et 25 !"
-print "> "
-  num = gets.chomp.to_i
+
+	print "> "
+	num = gets.chomp.to_i
 end
-d=1
+
+#Fin du loop pour le chiffre entre 1 et 25
+
 puts "Voici la pyramide :"
+
+# Debut du loop pour l'affichage de la pyramide
+
+d=1
 while d < num+1
-  (num-d).times do
-    print " "
-  end
-  d.times do
-    print "#"
-  end
-  puts " "
-  d+=1
+	(num-d).times do
+    	print " "
+	end
+	d.times do
+    	print "#"
+	end
+	puts " "
+	d+=1
 end
+
+# Fin du loop pour l'affichage de la pyramide
